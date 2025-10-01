40219036112069b592e96729b4e2d378afe8cc5b
Ed2uiz <ecr7407@gmail.com>
2025-10-01 10:57:20 -0400

    chore: update pkg improts

--- R/package_imports.R
+++ R/package_imports.R
@@ -1,5 +1,7 @@
 #' @import GiottoUtils
 #' @importMethodsFrom GiottoClass calculateOverlap createGiottoPoints createGiottoPolygon overlapToMatrix
+#' @importClassesFrom Matrix Matrix
+#' @importClassesFrom dbMatrix dbMatrix
 #' @importFrom data.table ":="
 #' @importFrom dbMatrix rowVars colVars rowMeans colMeans rowSums
 #' @importFrom stats var quantile predict
