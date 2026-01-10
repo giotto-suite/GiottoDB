43aeae065a668dffd95aba56ffe9cc7ae75ecd04
Ed2uiz <ecr7407@gmail.com>
2026-01-10 18:25:33 -0500

    chore: import dbSpatial class and rename file to `imports.R`

--- R/imports.R
+++ R/imports.R
@@ -2,6 +2,7 @@
 #' @importMethodsFrom GiottoClass calculateOverlap createGiottoPoints createGiottoPolygon overlapToMatrix
 #' @importClassesFrom Matrix Matrix
 #' @importClassesFrom dbMatrix dbMatrix
+#' @importClassesFrom dbSpatial dbSpatial
 #' @importFrom data.table ":="
 #' @importFrom dbMatrix rowVars colVars rowMeans colMeans rowSums colSums
 #' @importFrom stats var quantile predict
