2107d662f8f6673ef9714995c99fdbdbfb14e38e
Ed2uiz <ecr7407@gmail.com>
2026-01-25 17:42:44 -0500

    feat: support weighted overlapToMatrix for dbSpatial objects

--- R/overlapToMatrix.R
+++ R/overlapToMatrix.R
@@ -8,6 +8,7 @@ setMethod(
     x,
     col_names = NULL,
     row_names = NULL,
+    feat_count_column = NULL,
     count_info_column = NULL,
     output = c("dbSparseMatrix", "Matrix", "data.table"),
     verbose = TRUE,
@@ -21,35 +22,41 @@ setMethod(
     # NSE vars
     poly_ID <- NULL
 
+    # Backwards compatibility with older argument name
+    if (is.null(feat_count_column) && !is.null(count_info_column)) {
+      feat_count_column <- count_info_column
+    }
+
     # 1. convert to DT and cleanup
     # not needed because NA values are automatically removed in DB
 
-    # 2. Perform aggregation to counts DT
-    if (!is.null(count_info_column)) {
-      #TODO
-      stop("Not yet implemented for dbSpatial objects")
+    # 2. Perform aggregation
+    tmp_name <- paste0("dbMatrix_", paste(sample(LETTERS, 9), collapse = ""))
+    if (!is.null(feat_count_column) && !isFALSE(feat_count_column)) {
+      if (!feat_count_column %in% colnames(x[])) {
+        stop(
+          "feat_count_column '",
+          feat_count_column,
+          "' not found in overlap table",
+          call. = FALSE
+        )
+      }
 
-      # if (!count_info_column %in% colnames(dtoverlap)) {
-      #     .gstop("count_info_column ", count_info_column,
-      #         " does not exist",
-      #         .n = 2L
-      #     )
-      # }
+      # Coerce to numeric to ensure SUM() works on DB backends
+      tbl_weighted <- x[] |>
+        dplyr::mutate(.db_count = as.numeric(.data[[feat_count_column]]))
 
-      # # aggregate counts of features
-      # dtoverlap[, c(count_info_column) := as.numeric(
-      #     get(count_info_column)
-      # )]
-      # aggr_dtoverlap <- dtoverlap[, base::sum(get(count_info_column)),
-      #     by = c("poly_ID", "feat_ID")
-      # ]
-      # data.table::setnames(aggr_dtoverlap, "V1", "N")
+      aggr_dtoverlap <- dbMatrix::dbMatrix_from_tbl(
+        tbl = tbl_weighted,
+        rownames_colName = "feat_ID",
+        colnames_colName = "poly_ID",
+        value_colName = ".db_count",
+        name = tmp_name,
+        overwrite = TRUE
+      )
     } else {
-      # if no counts col
-      # aggregate individual features
-      tmp_name <- paste0('dbMatrix_', paste(sample(LETTERS, 9), collapse = ''))
-      aggr_dtoverlap <- dbMatrix:::dbMatrix_from_tbl(
-        #FIXME: export this function
+      # Unweighted: count occurrences per (feat_ID, poly_ID)
+      aggr_dtoverlap <- dbMatrix::dbMatrix_from_tbl(
         tbl = x[],
         rownames_colName = "feat_ID",
         colnames_colName = "poly_ID",
@@ -85,7 +92,6 @@ setMethod(
         stop("Not yet implemented for dbSpatial objects")
       },
       "MATRIX" = {
-        #TODO: throw warning before casting for possible memory failure
         # dbMatrix intentionally errors on 1x1 objects for as.matrix();
         # downstream expects a matrix-like object, so special-case scalars.
         if (all(dim(aggr_dtoverlap) == c(1L, 1L))) {
@@ -101,6 +107,18 @@ setMethod(
           res <- as(as(res, "generalMatrix"), "CsparseMatrix")
         } else {
           res <- as.matrix(aggr_dtoverlap, sparse = TRUE, names = TRUE)
+        }
+
+        if (!is.null(col_names) && !is.null(row_names)) {
+          full <- Matrix::Matrix(
+            0,
+            nrow = length(row_names),
+            ncol = length(col_names),
+            sparse = TRUE,
+            dimnames = list(row_names, col_names)
+          )
+          full[rownames(res), colnames(res)] <- res
+          res <- full
         }
         return(res)
       }
